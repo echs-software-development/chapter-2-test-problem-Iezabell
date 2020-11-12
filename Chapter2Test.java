322a967adf7e07555e7e9f62569624edaf518177
Iezabell <ic4148@necsd.net>
2020-11-12 15:11:08 +0000

    I finished the test

--- Chapter2Test.java
+++ Chapter2Test.java
@@ -1,7 +1,16 @@
-
-
+import java.util.Scanner;
 public class Chapter2Test {
   public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Please enter two numbers: ");
+    double numberone = scan.nextDouble();
+    double numbertwo = scan.nextDouble();
+
+    System.out.println("The average is " + (numberone + numbertwo) / 2);
+
+
   
   }
 }
